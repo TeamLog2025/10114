8328f5774ceaf708d78c6e54669a7288158aee75
dltmd03 <dltmdwls0307@gmail.com>
2025-06-18 12:00:57 +0900

    Create 2번.js

---
+++ 2번.js
@@ -0,0 +1,5 @@
+const path = require( 'path');
+const filePath = `/Users/student/project/test.js`;
+console.log( '디렉터리명:', path.dirname(filePath));
+console.Log('파일명:', path.basename(fttePath));
+console.log('확장자:', path.extname(filePath));
